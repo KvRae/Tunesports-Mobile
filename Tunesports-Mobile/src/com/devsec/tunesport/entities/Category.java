33c4b7afa7fe0b8b2197d148b93766f856d8bb80
KvRae <58667227+KvRae@users.noreply.github.com>
2022-04-29 14:45:57 +0100

    Setting up my entities

--- Tunesports-Mobile/src/com/devsec/tunesport/entities/Category.java
+++ Tunesports-Mobile/src/com/devsec/tunesport/entities/Category.java
@@ -1,4 +1,50 @@
 package com.devsec.tunesport.entities;
 
 public class Category {
+    private int id ;
+    private String name_cat ;
+    private String desc_cat ;
+
+    public Category(int id, String name_cat , String desc_cat ) {
+        this.id = id;
+        this.name_cat = name_cat;
+        this.desc_cat = desc_cat;
+    }
+
+    public Category(String name_cat, String desc_cat ) {
+        this.name_cat = name_cat;
+        this.desc_cat = desc_cat;
+    }
+
+    public Category() {
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomCat() {
+        return name_cat;
+    }
+    public void setNomCat(String name_cat) {
+        this.name_cat = name_cat;
+    }
+    public String getDescCat() {
+        return desc_cat;
+    }
+    public void setDescCat(String desc_cat) {
+        this.desc_cat = desc_cat;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "id=" + id + ", name_cat=" + name_cat + ", desc_cat=" + desc_cat + '}';
+    }
+
 }
+
--- Tunesports-Mobile/src/com/devsec/tunesport/entities/Reservation.java
+++ Tunesports-Mobile/src/com/devsec/tunesport/entities/Reservation.java
@@ -1,4 +1,80 @@
 package com.devsec.tunesport.entities;
 
+import java.util.Date;
+
 public class Reservation {
+    private int idRes ;
+    private int idCli ;
+    private int idCoach;
+    private Date dateRes;
+    private String dispoRes;
+    private double prixRes ;
+
+//***********************************************************Constructors*********************************
+
+    public Reservation() {
+    }
+
+    public Reservation(Date dateRes, String dispoRes, double prixRes) {
+        this.dateRes = dateRes;
+        this.dispoRes = dispoRes;
+        this.prixRes = prixRes;
+    }
+
+    public Reservation(int idRes, Date dateRes, String dispoRes, double prixRes) {
+        this.idRes = idRes;
+        this.dateRes = dateRes;
+        this.dispoRes = dispoRes;
+        this.prixRes = prixRes;
+    }
+
+    public Reservation(int idRes) {
+        this.idRes = idRes;
+    }
+
+    //***********************************************Getters&Setters**********************************************
+
+    public int getIdRes() {
+        return idRes;
+    }
+
+    public void setIdRes(int idRes) {
+        this.idRes = idRes;
+    }
+
+    public Date getDateRes() {
+        return dateRes;
+    }
+
+    public void setDateRes(Date dateRes) {
+        this.dateRes = dateRes;
+    }
+
+    public double getPrixRes() {
+        return prixRes;
+    }
+
+    public void setPrixRes(double prixRes) {
+        this.prixRes = prixRes;
+    }
+
+    public String getDispoRes() {
+        return dispoRes;
+    }
+
+    public void setDispoRes(String dispoRes) {
+        this.dispoRes= dispoRes;
+    }
+
+//***********************************************ToString = afficher **********************************************
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "idRes=" + idRes +
+                ", dateRes='" + dateRes +
+                ", dispoRes=" + dispoRes +
+                ", prixRes=" + prixRes +
+                '}';
+    }
 }
